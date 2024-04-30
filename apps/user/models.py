262992a560720d41d73a2c83a7893f5cc6ae2f1f
MaksutovOmurbek <maksutovomurbek164@gmail.com>
2024-04-30 19:55:31 +0300

    create validate

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(AbstractUser):
@@ -13,3 +14,11 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class YourModel(models.Model):
+    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\(\+996\)\d{9}$')])
+
+    def save(self, *args, **kwargs):
+       
+        super().save(*args, **kwargs)
